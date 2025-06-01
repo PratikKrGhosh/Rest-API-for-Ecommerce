f47df4b99d33500dea66e7f85ffc317708c1eb70
PratikKrGhosh <work.pratikkrghosh@gmail.com>
2025-06-02 04:21:42 +0530

    Created CRUD feature for products api with sorting and advance search

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 // impoerting all files and libraries
-import express from "express";
+import express, { urlencoded } from "express";
 import config from "./config/env.config.js";
 import { db } from "./config/db.config.js";
+import productsRoute from "./route/products.route.js";
 
 // Creating all constants
 const app = express();
 const port = config.port || 3000;
 
 // all middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 db();
+app.use("/api/products", productsRoute);
 
 // listening to the app
 app.listen(port, ()=>{console.log(`App is running at Port: ${port}`)});
---
+++ model/products.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    "name": {
+        "type": String,
+        "required": [true, "Enter The Product Name"]
+    },
+    "price": {
+        "type": Number,
+        "required": [true, "Enter The Product Price"]
+    },
+    "desc": {
+        "type": String,
+        "required": [true, "Enter The Product Description"]
+    },
+    "available": {
+        "type": Boolean,
+        "default": true
+    },
+    "launch": {
+        "type": Date,
+        "default": Date.now()
+    }
+})
+
+export const Product = new mongoose.model('product', productSchema);
